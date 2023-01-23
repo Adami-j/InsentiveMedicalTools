b245f39a3ed463be3f4a475a62a97f7390ca9bc6
mkltr <mkltr309@gmail.com>
2023-01-23 23:10:07 +0100

    j1.8->11

--- src/main/java/com/example/insentivetest/modele/utilisateur/Utilisateur.java
+++ src/main/java/com/example/insentivetest/modele/utilisateur/Utilisateur.java
@@ -1,6 +1,11 @@
 package com.example.insentivetest.modele.utilisateur;
 
-import jakarta.persistence.*;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.time.LocalDate;
 
